7105f2c6e28f2cd18787700ff647c97aa04ee773
zaaack <zaaack@qq.com>
2019-01-10 22:22:38 +0800

    fix ci

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './exec'
-export * from './fs'
-export * from './logger'
+export { fs } from './fs'
+export { logger } from './logger'
 export { task, desc, option, strict, setOption, setGlobalOptions, Dependency, Task, TaskFn, TaskDep, GlobalOptions } from './task'
 export { sleep, throttle, defaults } from './utils'
-export { } // try fix ci
