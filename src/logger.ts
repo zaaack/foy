c814a40ec930b35c5a383f4c1d71108f057ae465
zaaack <zaaack@qq.com>
2019-07-08 15:30:08 +0800

    refactor: logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,27 +1,56 @@
 import chalk from 'chalk'
 import { getGlobalTaskManager } from './task-manager'
+import * as util from 'util'
 
-const Levels = {
+export const LogLevels = {
   debug: 0,
   info: 1,
   warn: 2,
   error: 3,
 }
-function makeLogger(level: keyof (typeof Levels), color: (s: string) => string) {
+function makeLogger(
+  level: keyof (typeof LogLevels),
+  color: (s: string) => string,
+  props: ILoggerProps
+) {
   return (...args: any[]) => {
-    let levelNum = Levels[level]
+    let levelNum = LogLevels[level]
     let taskManager = getGlobalTaskManager()
-    let curLevelNum = Levels[taskManager.globalOptions.logLevel || 'debug']
+    let curLevelNum = LogLevels[taskManager.globalOptions.logLevel || 'debug']
     if (levelNum >= curLevelNum) {
       console.log(color(`[${level}]`), ...args)
+      props.onLog && props.onLog({
+        level,
+        message: args.map(a => util.inspect(a, false, null)).join(' '),
+        levelNum,
+        args,
+      })
     }
   }
 }
 
-export const logger = {
-  debug: makeLogger('debug', chalk.blueBright),
-  info: makeLogger('info', chalk.green),
-  log: makeLogger('info', chalk.green),
-  warn: makeLogger('warn', chalk.yellow),
-  error: makeLogger('error', chalk.red),
+export interface ILogInfo {
+  level: keyof (typeof LogLevels)
+  message: string
+  levelNum: number
+  args: any[]
 }
+
+export interface ILoggerProps {
+  onLog?(info: ILogInfo): void
+}
+
+export class Logger {
+  constructor(
+    /** @internal */
+    public _props: ILoggerProps = {}
+  ) {
+  }
+  debug = makeLogger('debug', chalk.blueBright, this._props)
+  info = makeLogger('info', chalk.green, this._props)
+  log = makeLogger('info', chalk.green, this._props)
+  warn = makeLogger('warn', chalk.yellow, this._props)
+  error = makeLogger('error', chalk.red, this._props)
+}
+
+export const logger = new Logger()
