6ce87507c2465b8aa85d76067a7b59d5a91e7983
zaaack <zaaack@qq.com>
2024-08-26 21:14:08 +0800

    fix: register

--- src/cli.ts
+++ src/cli.ts
@@ -23,22 +23,22 @@ async function main() {
         env = 'ts-node'
       } else if ('tsx' in deps) {
         env = 'tsx'
-      } else if ('@swc/register' in deps) {
+      } else if ('@swc-node/register' in deps) {
         if (isESM) {
           registers.push('@swc-node/register/esm-register')
         } else {
-          registers.push('@swc/register')
+          registers.push('@swc-node/register')
         }
       } else if ('swc-node' in deps) {
         env = 'swc-node'
       } else {
-        logger.error('no ts-node or tsx or swc-node found')
+        logger.error('no ts-node/tsx/swc-node or @swc-node/register found')
         process.exit(1)
       }
     }
     const args = [
-      foyFile,
       ...registers.map((r) => `--${isESM ? 'import' : 'require'} ${r}`),
+      foyFile,
       ...process.argv.slice(2),
     ]
     spawn(env, args, {
