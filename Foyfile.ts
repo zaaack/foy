d583b5ea219b72fdc5e2aa5e8f8e1d9db6fd3fcc
zaaack <zaaack@qq.com>
2026-07-09 12:39:32 +0800

    chore: ts v7

--- Foyfile.ts
+++ Foyfile.ts
@@ -43,7 +43,7 @@ task('build', async (ctx) => {
 desc('generate doc')
 task('doc', async (ctx) => {
   await fs.rmrf('./docs/api/')
-  await ctx.exec(`typedoc --excludePrivate  --out ./docs/api ./src/index.ts`)
+  await ctx.exec(`npx --yes --package typedoc@0.26.11 --package typescript@5.5.4 typedoc --excludePrivate --out ./docs/api ./src/index.ts`)
   await ctx.exec(`touch ./docs/.nojekyll`)
 })
 
